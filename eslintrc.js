e6824f08fe241e0d5a5ffe6efced4c139d8d6e5d
TomaAlinVasile <149952434+TomaAlinVasile@users.noreply.github.com>
2023-12-04 08:04:52 +0200

    Add files via upload

---
+++ eslintrc.js
@@ -0,0 +1,7 @@
+{
+  "extends": ["react-app", "react-app/jest"],
+  "rules": {
+    // Add any other rules or overrides as needed
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+  }
+}
